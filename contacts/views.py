41c267e17df6bbaf3d87d3352d1fd363022ddcfa
egrtumc <egrtumc@hotmail.com>
2020-05-01 19:21:45 +0700

    'email'

--- contacts/views.py
+++ contacts/views.py
@@ -29,14 +29,14 @@ def contact(request):
 
         contact.save()
 
-        #Send email
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '. Sign into the admin panel for moore info',
-            'testserverdeveloper@gmail.com',
-            [realtor_email, 'egrtumc29@gmail.com','egrtumc@hotmail.com'],
-            fail_silently=False
-        )
+        # #Send email
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing + '. Sign into the admin panel for moore info',
+        #     'testserverdeveloper@gmail.com',
+        #     [realtor_email, 'egrtumc29@gmail.com','egrtumc@hotmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(request,'Your request has been submitted, a realtor will get to ur soon')
         return redirect('/listings/'+ listing_id)
