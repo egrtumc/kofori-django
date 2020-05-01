ae71181e63ba72a37c279c947cac49c5dae2a2b6
egrtumc <egrtumc@hotmail.com>
2020-05-01 18:28:51 +0700

    'try'

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'testserverdeveloper@gmail.com'
 EMAIL_HOST_PASSWORD = 'port3000'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
